015b60b119e24c04cb78dc43ec8459f0c1a367e6
rostyslavhereha <rostyslav.hereha@lvairo.com>
2024-03-09 18:14:09 +0200

    Add test for FaceLockServer

--- app/client.py
+++ app/client.py
@@ -2,7 +2,7 @@ import socket
 import face_recognition
 import pickle
 import numpy as np
-
+import sys
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 9000
 
@@ -13,15 +13,14 @@ class FaceLockClient:
 
     def send_message(self, message):
         message = pickle.dumps(message)
-        print(f"data: {message}")
         self.client.send(message)
         response = self.client.recv(8192)
         if response:
             response = pickle.loads(response)
             if response['status'] == 200:
-                print(response)
+                return response
             else:
-                print(response)
+                return response
 
     def __del__(self):
         self.client.close
@@ -47,26 +46,3 @@ class RegisterUserMessage:
     def get_data(self):
         return self.__dict__()
 
-if __name__ == "__main__":
-    client = FaceLockClient()
-    unknown_image = face_recognition.load_image_file("Biden.png")
-    import sys
-    biden_encoding = face_recognition.face_encodings(unknown_image)[0]
-    print(f"type: {type(biden_encoding)}\n")
-    print(f"{sys.getsizeof(pickle.dumps('REGISTER_USER'))}")
-    tmp = biden_encoding.tobytes()
-    res = np.frombuffer(tmp)
-    print(f"test: {res == biden_encoding}\n"
-          f"type : {type(res)}")
-
-    message = RegisterUserMessage(
-        username="Joe Biden",
-        password="12345",
-        encode_data=biden_encoding
-
-    )
-    data = message.get_data()
-    tmp = pickle.dumps(data)
-    tmp = pickle.loads(tmp)
-    client.send_message(message.get_action())
-    client.send_message(message.get_data())
